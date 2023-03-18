d685d01de602c13014b3650b252f0d41a7bd2b42
橋爪優果 <hashizumeyuka@hashidzumeyuukanoMacBook-Pro.local>
2023-03-19 00:45:52 +0900

    revise js”

--- abx.js
+++ abx.js
@@ -252,7 +252,7 @@ function exportCSV() {
     }
 
     var csvData = "";
-    csvData += "" + "anchor" + "A" + "," + "B" + "," + "score" + "," + "ans" + "\r\n";
+    csvData += "" + "anchor" + "," + "A" + "," + "B" + "," + "score" + "," + "ans" + "," + "ABans" + "\r\n";
     for (var i = 0; i < file_list.length; i++) {
         csvData += "" + file_list[i][0] + "," + file_list[i][1] + ","
             + file_list[i][2] + ","
