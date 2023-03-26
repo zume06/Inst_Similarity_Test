5da02ac12bf5e957565d2ba17f75e9e7d6afe52c
橋爪優果 <hashizumeyuka@hashidzumeyuukanoMacBook-Pro.local>
2023-03-26 23:51:47 +0900

    revise js

--- abx.js
+++ abx.js
@@ -108,7 +108,7 @@ function makeFileList(method_list_path) {
     }
 
     var files = Array();
-    for (var i = 0; i < method[0].length + 1; i++) {
+    for (var i = 0; i < method[0].length; i++) {
         // TODO: automate here
         /*
             you have to customize this part
